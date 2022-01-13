743c38d18935bb3b7d2af316f4a4e125ffe6c174
Fridabolin <80760258+Fridabolin@users.noreply.github.com>
2022-01-13 10:42:57 +0100

    populerar url til account med namn samt välkomsttext

--- webshop-app/src/context/UserContext.js
+++ webshop-app/src/context/UserContext.js
@@ -4,10 +4,13 @@ export const UserContext = createContext();
 
 const UserProvider = ({children}) => {
   const [loggedIn, setLoggedIn] = useState(true);
- 
+
+  const [user, setUser] = useState({firstname: ""})
+
+  
   return (
     <>
-      <UserContext.Provider value={{ loggedIn, setLoggedIn}}>
+      <UserContext.Provider value={{ loggedIn, setLoggedIn, user, setUser, }}>
         {children}
       </UserContext.Provider>
     </>
