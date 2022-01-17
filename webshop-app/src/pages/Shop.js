26351a288c5bf58c4e7b60b4c7cf68c1127c8c36
Amalia <74189129+chocolateprincess666@users.noreply.github.com>
2022-01-17 18:30:13 +0100

    la tll produkter

--- webshop-app/src/pages/Shop.js
+++ webshop-app/src/pages/Shop.js
@@ -1,32 +1,26 @@
 import './CSS/Shop.css';
+import products from '../services/products';
 
 const Shop = () => {
 
 
     return (
-        <div className='container'>
             <div className='wrapper-products'>
 
-                <div className='card'>
-                    <div>
-                        <img className="img" src="#"/>
+                {products.map((item) => (
+                    <div className='cards'>
+                    <div className='card'>
+                        <img className="img" src={item.img} alt={item.title}/>
                             <div className='card-txts'>
-                                <h3 className='card-text'>title</h3>
-                                <h4 className='card-text'>price</h4>
+                                <h3 className='card-text'>{item.title}</h3>
+                                <h4 className='card-text'>${item.price}</h4>
                             </div>
                             <button className='btn-card'>Add to Cart</button>
+                        </div>
                     </div>
-                </div>          
+                ))}
+                          
             </div>
-
-            <div className='wrapper-cart'>
-                <div className='cart'>
-                    <div>
-                        <h2>Cart</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
     )
 }
 
---
+++ webshop-app/src/services/products.js
@@ -0,0 +1,79 @@
+
+
+const products = [
+    {
+        title: "Vincent Van Gogh - Starry night",
+        price: "200 000",
+        img: "https://upload.wikimedia.org/wikipedia/commons/c/c4/Vincent_van_Gogh%E2%80%99s_famous_painting%2C_digitally_enhanced_by_rawpixel-com_46.jpg",
+        id: 1
+    },
+    {
+        title: "Leonardo Da Vinci - Mona Lisa",
+        price: "200 000",
+        img: "https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/pdfma6-pdfamouspainting002001-image-8_8.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=aabe598fd7acd2280e580f37c9fd46de",
+        id: 2
+    },
+    {
+        title: "Edvard Much -  The Scream",
+        price: "200 000",
+        img: "https://upload.wikimedia.org/wikipedia/commons/3/34/Edvard-Munch-The-Scream.jpg",
+        id: 3
+    },
+    {
+        title: "Jackson Pollock - Number 17A",
+        price: "200 000",
+        img: "https://live.staticflickr.com/1661/23936923029_78631a84a8_b.jpg",
+        id: 4
+    },
+    {
+        title: "Salvador Dali - The Persistance of...",
+        price: "200 000",
+        img: "https://live.staticflickr.com/3956/15694508911_30fc70b1e1_b.jpg",
+        id: 5
+    },
+    {
+        title: "Claude Monet - The Japanese Footbridge",
+        price: "200 000",
+        img: "https://live.staticflickr.com/7181/7034361895_94dec797d7_b.jpg",
+        id: 6
+    },
+    {
+        title: "Pablo Picasso - Head of a Woman",
+        price: "200 000",
+        img: "https://live.staticflickr.com/65535/50802928326_4a6baa1606_b.jpg",
+        id: 7
+    },
+    {
+        title: "William Turner -  The Shipwreck",
+        price: "200 000",
+        img: "https://cdn2.picryl.com/photo/2021/02/07/joseph-mallord-william-turner-the-shipwreck-google-art-project-1432bc-1024.jpg",
+        id: 8
+    },
+    {
+        title: "Salvador Dali - Metamorphosis of...",
+        price: "200 000",
+        img: "https://live.staticflickr.com/5127/5311661578_5cd32585f6_b.jpg",
+        id: 9
+    },
+    {
+        title: "Vincent Van Gogh - Self Portrait 1887",
+        price: "200 000",
+        img: "https://upload.wikimedia.org/wikipedia/commons/f/fc/Vincent_van_Gogh%E2%80%99s_famous_painting%2C_digitally_enhanced_by_rawpixel-com_44.jpg",
+        id: 10
+    },
+    {
+        title: "Jacques Louis David - The Oath of...",
+        price: "200 000",
+        img: "https://cdn2.picryl.com/photo/1786/12/31/jacques-louis-david-oath-of-the-horatii-google-art-project-87aab0-1024.jpg",
+        id: 11
+    },
+    {
+        title: "Dante Gabriel Rossetti - Joan of...",
+        price: "200 000",
+        img: "https://freesvg.org/img/Joan-of-Arc-1882-Dante-Gabriel-Rossetti.png",
+        id: 12
+    },
+
+]
+
+export default products;
