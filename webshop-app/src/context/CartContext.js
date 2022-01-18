ba62edf95455d29dabfe4e8e5e5a89e304cbddaf
Essmea <maiaruiz@live.se>
2022-01-18 13:58:00 +0100

    Maias

---
+++ webshop-app/src/context/CartContext.js
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext();
+
+export default function CartProvider({children}) {
+
+    const [cart, setCart] = useState([]);
+    const [cartVisible, setCartVisible] = useState(false)
+
+  return(
+      <>
+        <CartContext.Provider value={{ cart, setCart, cartVisible, setCartVisible }}>
+            {children}
+        </CartContext.Provider>
+      </>
+  )  
+}
